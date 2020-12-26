1dc88302fd19b80ac4c33304ba954e30e024caab
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-12-26 23:22:02 +0500

    add new folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path")
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPost = path.resolve(`src/template/blog-template.tsx`)
+  const result = await graphql(`
+    {
+      blog: allContentfulPost {
+        edges {
+          node {
+            title
+            subtitle
+            author
+            slug
+            image {
+              fluid {
+                src
+              }
+            }
+            content {
+              raw
+            }
+          }
+        }
+      }
+    }
+  `)
 
-// You can delete this file if you're not using it
+  result.data.blog.edges.nodes.forEach(product => {
+    console.log(product, "PRODUCT")
+    createPage({
+      path: `/products/${product.slug}`,
+      component: blogPost,
+      context: {
+        slug: product.slug,
+      },
+    })
+  })
+}
