cb4e043525bac1b2c30660ab16a258e977ddc7c2
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-01-15 10:13:33 +0500

    some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv")
-
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config()
-}
+dotenv.config()
+// if (process.env.NODE_ENV !== "production") {
+//   dotenv.config()
+// }
 module.exports = {
   siteMetadata: {
     title: `Blog Codes`,
